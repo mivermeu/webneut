c29661b60290d97fffd58eba065f0ce1c8e0ee43
Milo Vermeulen <milovermeulen@me.com>
2021-03-08 18:09:45 +0100

    Clean code

--- Oscillator.js
+++ Oscillator.js
@@ -3,33 +3,43 @@ var makeParStruct = ({
   nu = 1, // Initial neutrino flavour. 0=e, 1=mu, 2=tau
   anti = false, // Antineutrino or not.
   E = 1, // In GeV.
-  L = 33060.7*E, // PI / (1.267*Dm21sq) * E (Full Dm12sq period.) In km.
+  L = 33060.7 * E, // PI / (1.267*Dm21sq) * E (Full Dm12sq period.) In km.
   th12 = 0.5843,
   th23 = 0.738,
   th13 = 0.148,
   Dm21sq = 7.5, // In 10^-5 eV^2
   Dm31sq = 2.457, // In 10^-3 eV^2
   dCP = 1.38 * 3.14159265,
-  rho = 0 // In kg/m^3
+  rho = 0, // In kg/m^3
 } = {}) => ({
-  nu, anti, E, L, th12, th23, th13, Dm21sq, Dm31sq, dCP, rho
+  nu,
+  anti,
+  E,
+  L,
+  th12,
+  th23,
+  th13,
+  Dm21sq,
+  Dm31sq,
+  dCP,
+  rho,
 });
 
 function makeParMap() {
-  var pars = new Map()
+  var pars = new Map();
 
   pars.set("nsteps", 300); // Initial neutrino flavour. 0=e, 1=mu, 2=tau
-  pars.set("nu",     1); // Initial neutrino flavour. 0=e, 1=mu, 2=tau
-  pars.set("anti",   false); // Antineutrino or not.
-  pars.set("E",      1); // In GeV.
-  pars.set("L",      [0,33060.7]); // PI / (1.267*Dm21sq) * 1 (Full Dm12sq period.) In km.
-  pars.set("th12",   0.5843);
-  pars.set("th23",   0.738);
-  pars.set("th13",   0.148);
+  pars.set("nu", 1); // Initial neutrino flavour. 0=e, 1=mu, 2=tau
+  pars.set("anti", false); // Antineutrino or not.
+  pars.set("E", 1); // In GeV.
+  pars.set("L", [0, 33060.7]); // PI / (1.267*Dm21sq) * 1 (Full Dm12sq period.) In km.
+  pars.set("th12", 0.5843);
+  pars.set("th23", 0.738);
+  pars.set("th13", 0.148);
   pars.set("Dm21sq", 7.5); // In 10^-5 eV^2
   pars.set("Dm31sq", 2.457); // In 10^-3 eV^2
-  pars.set("dCP",    1.38 * 3.14159265);
-  pars.set("rho",    0); // In kg/m^3
+  pars.set("dCP", 1.38 * 3.14159265);
+  pars.set("rho", 0); // In kg/m^3
 
   return pars;
 }
@@ -43,41 +53,53 @@ function Matrices(pars) {
   this.c13 = math.cos(pars.get("th13"));
 
   this.ch = 1;
-  if(pars.get("anti")) this.ch = -1;
+  if (pars.get("anti")) this.ch = -1;
 
-  this.edcp = math.exp(math.complex(0,this.ch*pars.get("dCP")));
-  this.emdcp = math.pow(this.edcp,-1);
+  this.edcp = math.exp(math.complex(0, this.ch * pars.get("dCP")));
+  this.emdcp = math.pow(this.edcp, -1);
 
   // Construct oscillation matrix.
-  this.U1 = math.matrix([[1, 0, 0],
-                          [0, this.c23, this.s23],
-                          [0, -this.s23, this.c23]]);
+  this.U1 = math.matrix([
+    [1, 0, 0],
+    [0, this.c23, this.s23],
+    [0, -this.s23, this.c23],
+  ]);
 
-  this.U2 = math.matrix([[this.c13, 0, math.multiply(this.s13,this.emdcp)],
-                          [0, 1, 0],
-                          [math.multiply(-this.s13,this.edcp), 0, this.c13]]);
-  
-  this.U3 = math.matrix([[this.c12, this.s12, 0],
-                          [-this.s12, this.c12, 0],
-                          [0, 0, 1]]);
+  this.U2 = math.matrix([
+    [this.c13, 0, math.multiply(this.s13, this.emdcp)],
+    [0, 1, 0],
+    [math.multiply(-this.s13, this.edcp), 0, this.c13],
+  ]);
 
-  this.U = math.multiply(this.U1,this.U2,this.U3);
+  this.U3 = math.matrix([
+    [this.c12, this.s12, 0],
+    [-this.s12, this.c12, 0],
+    [0, 0, 1],
+  ]);
+
+  this.U = math.multiply(this.U1, this.U2, this.U3);
   this.Ud = math.ctranspose(this.U);
 
   // Hamiltonian.
-  this.H = math.matrix([[0,0,0],
-                        [0, pars.get("Dm21sq")*1e-5, 0],
-                        [0, 0, pars.get("Dm31sq")*1e-3]]);
+  this.H = math.matrix([
+    [0, 0, 0],
+    [0, pars.get("Dm21sq") * 1e-5, 0],
+    [0, 0, pars.get("Dm31sq") * 1e-3],
+  ]);
 }
 
 function oscillate(p, m) {
-  var nu = [0,0,0];
+  var nu = [0, 0, 0];
   nu[p.get("nu")] = 1;
   // const conv = 2.534; // Conversion factor from natural to useful units.
-  const conv = 1e-6/(2*1.0545718e-34*299792458/(1.602e-19)); // Conversion factor from natural to useful units.
-  var Hexp = math.multiply(math.complex(0,1), -conv*p.get("L")/p.get("E"), m.H);
-  for(var j=0; j<3; ++j) Hexp._data[j][j] = math.exp(Hexp.get([j,j]));
-  const UHUdnu = math.multiply(m.U,Hexp,m.Ud,nu);
+  const conv = 1e-6 / ((2 * 1.0545718e-34 * 299792458) / 1.602e-19); // Conversion factor from natural to useful units.
+  var Hexp = math.multiply(
+    math.complex(0, 1),
+    (-conv * p.get("L")) / p.get("E"),
+    m.H
+  );
+  for (var j = 0; j < 3; ++j) Hexp._data[j][j] = math.exp(Hexp.get([j, j]));
+  const UHUdnu = math.multiply(m.U, Hexp, m.Ud, nu);
   return math.re(math.dotMultiply(UHUdnu, math.conj(UHUdnu))).valueOf();
 }
 
@@ -133,7 +155,7 @@ function oscillate(p, m) {
 //     H << 0, 0, 0,
 //          0, op.Dm21sq, 0,
 //          0, 0, op.Dm31sq;
-    
+
 //     const double Gf = 4.54164e-37; // Reduced Fermi constant * (c*hbar)^2 in m^2.
 //     const double Ne = op.rho/(1.672e-27)/2; // Electron number density in m^-3.
 //     V(0,0) = ch*sqrt(2)*Gf*Ne * 1e3; // Multiply and convert to km^-1.
--- controlpanel.js
+++ controlpanel.js
@@ -1,4 +1,4 @@
-function addRow(panel, id, label, slimits, sstart, precision, snaps=[]) {
+function addRow(panel, id, label, slimits, sstart, precision, snaps = []) {
   // // Radio button.
   // let buttond = document.createElement("div");
   // buttond.innerText = "Hello there.";
@@ -7,13 +7,13 @@ function addRow(panel, id, label, slimits, sstart, precision, snaps=[]) {
   // Button.
   var buttond = document.createElement("button");
   buttond.innerHTML = "R";
-  buttond.id = id+"_button";
+  buttond.id = id + "_button";
   panel.appendChild(buttond).classname += "grid-item";
 
   // Label.
   let labeld = document.createElement("div");
   labeld.innerHTML = label;
-  labeld.id = id+"_label";
+  labeld.id = id + "_label";
   panel.appendChild(labeld).className += "grid-item";
 
   // Slider.
@@ -27,56 +27,58 @@ function addRow(panel, id, label, slimits, sstart, precision, snaps=[]) {
   panel.appendChild(inputs).className += "grid-item";
 
   return sliderd;
-};
+}
 
 function createSlider(el, slimits, sstart, precision, snaps) {
   noUiSlider.create(el, {
     start: sstart,
-    orientation: 'horizontal',
+    orientation: "horizontal",
     connect: true,
     animate: false,
     // behaviour: 'snap',
     range: {
-      'min': slimits[0],
-      'max': slimits[1]
+      min: slimits[0],
+      max: slimits[1],
     },
     format: {
-      'to': function( value ){ return value.toFixed(precision); },
-      'from': Number
+      to: function (value) {
+        return value.toFixed(precision);
+      },
+      from: Number,
     },
     pips: {
-      mode: 'values',
+      mode: "values",
       values: snaps,
       density: -1,
-      format: 0
-    }
+      format: 0,
+    },
   });
-};
+}
 
 function createInput(sliderd) {
   // Get slider start points.
   sstart = sliderd.noUiSlider.options.start;
   // Create as many input fields as slider knobs.
-  if(sstart.constructor != Array) sstart = [sstart];
+  if (sstart.constructor != Array) sstart = [sstart];
   var inputs = document.createElement("span");
-  sstart.forEach(function(_, handle) {
+  sstart.forEach(function (_, handle) {
     let thisinput = document.createElement("input");
     thisinput.style.width = "100px";
 
     // Update input field on slider change.
-    sliderd.noUiSlider.on('update', function(value) {
+    sliderd.noUiSlider.on("update", function (value) {
       thisinput.value = value[handle];
     });
 
     // Update slider on input field change.
-    thisinput.addEventListener('change', function() {
+    thisinput.addEventListener("change", function () {
       var values = [null, null];
       values[handle] = thisinput.value;
       sliderd.noUiSlider.set(values);
     });
 
-    thisinput.addEventListener('keydown', function(e) {
-      if(e==13) {
+    thisinput.addEventListener("keydown", function (e) {
+      if (e == 13) {
         sliderd.noUiSlider.set(this.value);
       }
     });
@@ -84,52 +86,55 @@ function createInput(sliderd) {
     inputs.appendChild(thisinput).className += "grid-item";
   });
   // Add input fields to span in grid cell.
-  inputs.id = sliderd.id+"_input";
+  inputs.id = sliderd.id + "_input";
 
   return inputs;
 }
 
 function setPars(newPars, sliders) {
-  for(const key in newPars) {
-    sliders.forEach(function(slider) {
-      if(key==slider.id) {
+  for (const key in newPars) {
+    sliders.forEach(function (slider) {
+      if (key == slider.id) {
         slider.noUiSlider.set(newPars[key]);
       }
     });
   }
-};
+}
 
 function makeRange(sliders, id) {
-  sliders.forEach(function(slider) {
+  sliders.forEach(function (slider) {
     // Find the (and any) previous range slider and remake it with a single handle.
-    if(Array.isArray(slider.noUiSlider.options.start)) {
+    if (Array.isArray(slider.noUiSlider.options.start)) {
       var ops = slider.noUiSlider.options;
       ops.start = slider.noUiSlider.get()[1];
       slider.noUiSlider.destroy();
       noUiSlider.create(slider, ops);
       // Remake input fields.
       let inputs = createInput(slider);
-      document.getElementById(slider.id+"_input").replaceWith(inputs);
+      document.getElementById(slider.id + "_input").replaceWith(inputs);
     }
     // Find the new range slider and remake it with two handles.
-    if(slider.id == id) {
+    if (slider.id == id) {
       var ops = slider.noUiSlider.options;
-      ops.start = [slider.noUiSlider.options.range['min'], slider.noUiSlider.get()];
+      ops.start = [
+        slider.noUiSlider.options.range["min"],
+        slider.noUiSlider.get(),
+      ];
       slider.noUiSlider.destroy();
       noUiSlider.create(slider, ops);
       // Remake input fields.
       let input = createInput(slider);
-      document.getElementById(slider.id+"_input").replaceWith(input);
+      document.getElementById(slider.id + "_input").replaceWith(input);
     }
   });
 }
 
 function mapCheckAndSetEqual(a, b) {
-  if(a.size != b.size) {
+  if (a.size != b.size) {
     return false;
   }
 
-  for(var key of a.keys()) {
+  for (var key of a.keys()) {
     if (a.get(key) != b.get(key)) {
       // Sets the value of the differing key to be equal between maps!
       b.set(key, a.get(key));
