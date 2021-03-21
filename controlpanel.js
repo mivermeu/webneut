6de63abb68e490603918c1350d07ceae2ef0d897
Milo Vermeulen <milovermeulen@me.com>
2021-03-21 23:39:01 +0100

    Clean code and introduce lodash as dependency

--- controlpanel.js
+++ controlpanel.js
@@ -1,9 +1,4 @@
 function addRow(panel, id, pars) {
-  // // Radio button.
-  // let buttond = document.createElement("div");
-  // buttond.innerText = "Hello there.";
-  // panel.appendChild(buttond).className += "grid-item";
-
   // Button.
   var buttond = document.createElement("button");
   buttond.innerHTML = "R";
@@ -57,7 +52,7 @@ function createSlider(el, slimits, sstart, precision, snaps = []) {
 
 function createInput(sliderd) {
   // Get slider start points.
-  sstart = sliderd.noUiSlider.options.start;
+  let sstart = sliderd.noUiSlider.options.start;
   // Create as many input fields as slider knobs.
   if (sstart.constructor != Array) sstart = [sstart];
   var inputs = document.createElement("span");
@@ -91,58 +86,31 @@ function createInput(sliderd) {
   return inputs;
 }
 
-function setPars(newPars, sliders) {
-  for (const key in newPars) {
-    sliders.forEach(function (slider) {
-      if (key == slider.id) {
-        slider.noUiSlider.set(newPars[key]);
-      }
-    });
-  }
-}
-
 function makeRange(sliders, id) {
-  sliders.forEach(function (slider) {
-    // Find the (and any) previous range slider and remake it with a single handle.
-    if (Array.isArray(slider.noUiSlider.options.start)) {
-      var ops = slider.noUiSlider.options;
-      ops.start = slider.noUiSlider.get()[1];
-      slider.noUiSlider.destroy();
-      noUiSlider.create(slider, ops);
-      // Remake input fields.
-      let inputs = createInput(slider);
-      document.getElementById(slider.id + "_input").replaceWith(inputs);
-    }
-    // Find the new range slider and remake it with two handles.
-    if (slider.id == id) {
-      var ops = slider.noUiSlider.options;
-      ops.start = [
-        slider.noUiSlider.options.range["min"],
-        slider.noUiSlider.get(),
-      ];
-      slider.noUiSlider.destroy();
-      noUiSlider.create(slider, ops);
-      // Remake input fields.
-      let input = createInput(slider);
-      document.getElementById(slider.id + "_input").replaceWith(input);
-    }
+  // Find range slider keys.
+  const rangesliders = sliders.filter((slider) =>
+    Array.isArray(slider.noUiSlider.options.start)
+  );
+  rangesliders.forEach(function (slider) {
+    // Remake range sliders with a single handle.
+    var ops = slider.noUiSlider.options;
+    ops.start = slider.noUiSlider.get()[1];
+    slider.noUiSlider.destroy();
+    noUiSlider.create(slider, ops);
+    // Remake input fields.
+    let inputs = createInput(slider);
+    document.getElementById(slider.id + "_input").replaceWith(inputs);
   });
-}
-
-function mapCheckAndSetEqual(a, b) {
-  if (a.size != b.size) {
-    return false;
-  }
-
-  for (var key in a) {
-    for(var kkey in a[key]) {
-      if (a[key][kkey] != b[key][kkey]) {
-        // Sets the value of the differing key to be equal between maps!
-        b[key][kkey] = a[key][kkey];
-        return false;
-      }
-    }
-  }
-
-  return true;
+  // Find the new range slider and remake it with two handles.
+  const newrangeslider = sliders.find((slider) => slider.id == id);
+  var ops = newrangeslider.noUiSlider.options;
+  ops.start = [
+    newrangeslider.noUiSlider.options.range["min"],
+    newrangeslider.noUiSlider.get(),
+  ];
+  newrangeslider.noUiSlider.destroy();
+  noUiSlider.create(newrangeslider, ops);
+  // Remake input fields.
+  let input = createInput(newrangeslider);
+  document.getElementById(newrangeslider.id + "_input").replaceWith(input);
 }
