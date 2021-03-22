d3131a0fcce20d8c129b53cc0d4158fb659238ea
Milo Vermeulen <milovermeulen@me.com>
2021-03-22 23:27:32 +0100

    Add matter effect oscillations and remove stale comments

--- Oscillator.js
+++ Oscillator.js
@@ -1,22 +1,3 @@
-// function makeParMap() {
-//   var pars = {};
-
-//   pars["nsteps"] = 300; // Initial neutrino flavour. 0=e, 1=mu, 2=tau
-//   pars["nu"] = 1; // Initial neutrino flavour. 0=e, 1=mu, 2=tau
-//   pars["anti"] = false; // Antineutrino or not.
-//   pars["E"] = 1; // In GeV.
-//   pars["L"] = [0, 33060.7]; // PI / (1.267*Dm21sq) * 1 (Full Dm12sq period.) In km.
-//   pars["th12"] = 0.5843;
-//   pars["th23"] = 0.738;
-//   pars["th13"] = 0.148;
-//   pars["Dm21sq"] = 7.5; // In 10^-5 eV^2
-//   pars["Dm31sq"] = 2.457; // In 10^-3 eV^2
-//   pars["dCP"] = 1.38 * 3.14159265;
-//   pars["rho"] = 0; // In kg/m^3
-
-//   return pars;
-// }
-
 function makeParMap() {
   return {
     nsteps: {
@@ -24,93 +5,95 @@ function makeParMap() {
       label: "Number of points",
       snaps: [],
       precision: 0,
-      limits: [1,1000]
+      limits: [1, 1000],
     },
     nu: {
       val: 1,
       label: "Neutrino flavour",
       snaps: [],
       precision: 0,
-      limits: [0,2]
+      limits: [0, 2],
     },
     anti: {
-      val: 0,
-      label: "Antineutrino",
+      val: 1,
+      label: "Chirality",
       snaps: [],
       precision: 0,
-      limits: [0,1]
+      limits: [-1, 1],
     },
     E: {
       val: 1,
       label: "Energy [GeV]",
       snaps: [],
       precision: 3,
-      limits: [0.3, 20]
+      limits: [0.3, 20],
     },
     L: {
-      val: [0,33060],
+      val: [0, 33060],
       label: "Path length [km]",
       snaps: [],
       precision: 0,
-      limits: [0, 40000]
+      limits: [0, 40000],
     },
     th12: {
       val: 0.5843,
       label: "\u03b8<sub>12</sub> [rad]",
       snaps: [0.5843, 3.14159265 / 2],
       precision: 4,
-      limits: [0, 3.1416]
+      limits: [0, 3.1416],
     },
     th23: {
       val: 0.738,
       label: "\u03b8<sub>23</sub> [rad]",
       snaps: [0.738, 3.14159265 / 2],
       precision: 4,
-      limits: [0, 3.1416]
+      limits: [0, 3.1416],
     },
     th13: {
       val: 0.148,
       label: "\u03b8<sub>13</sub> [rad]",
       snaps: [0.148, 3.14159265 / 2, 3.14159265, (3.14159265 / 2) * 3],
       precision: 4,
-      limits: [0, 2 * 3.1416]
+      limits: [0, 2 * 3.1416],
     },
     Dm21sq: {
       val: 7.5,
-      label: "\u0394m<sup>2</sup><sub>21</sub> [10<sup>-5</sup> eV<sup>2</sup>]",
+      label:
+        "\u0394m<sup>2</sup><sub>21</sub> [10<sup>-5</sup> eV<sup>2</sup>]",
       snaps: [7.5],
       precision: 4,
-      limits: [0,10]
+      limits: [0, 10],
     },
     Dm31sq: {
       val: 2.457,
-      label: "\u0394m<sup>2</sup><sub>31</sub> [10<sup>-5</sup> eV<sup>2</sup>]",
+      label:
+        "\u0394m<sup>2</sup><sub>31</sub> [10<sup>-5</sup> eV<sup>2</sup>]",
       snaps: [2.457, 0, -2.457],
       precision: 4,
-      limits: [-5, 5]
+      limits: [-5, 5],
     },
     dCP: {
       val: 0,
       label: "\u03b4<sub>CP</sub> [rad]",
       snaps: [-0.62 * 3.14159265, 0, 3.14159265 / 2, -3.14159265 / 2],
       precision: 4,
-      limits: [-3.1416, 3.1416]
+      limits: [-3.1416, 3.1416],
     },
     rho: {
       val: 0,
       label: "\u03c1 [kg/m<sup>3</sup>]",
       snaps: [2600],
       precision: 0,
-      limits: [0, 10000]
-    }
-  }
+      limits: [0, 10000],
+    },
+  };
 }
 
 class Matrices {
   constructor(pars) {
     this.update(pars);
   }
-  update = function(pars) {
+  update = function (pars) {
     this.s12 = math.sin(pars.th12.val);
     this.s23 = math.sin(pars.th23.val);
     this.s13 = math.sin(pars.th13.val);
@@ -152,13 +135,41 @@ class Matrices {
       [0, pars.Dm21sq.val * 1e-5, 0],
       [0, 0, pars.Dm31sq.val * 1e-3],
     ]);
+
+    const Gf = 4.54164e-37; // Reduced Fermi constant * (c*hbar)^2 in m^2.
+    const Ne = pars.rho.val / 1.672e-27 / 2; // Electron number density in m^-3.
+    this.V = math.matrix([
+      [pars.anti.val * math.sqrt(2) * Gf * Ne * 1e3, 0, 0], // Multiply and convert to km^-1.
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+  };
+}
+
+function oscillateRange(xv, p, m) {
+  // Use a temporary non-range parameter map to get oscillation results.
+  let tmppars = JSON.parse(JSON.stringify(pars));
+  for (let x of xv) {
+    tmppars[thisArrID].val = x;
+    if (rangeVarIsMixingPar) {
+      mat.update(tmppars);
+    }
+    yValues.push(oscillate(tmppars, mat));
+  }
+  yValues = math.transpose(yValues);
+}
+
+function oscillate(p, m, use_exp = false) {
+  if (Array.isArray(p.rho.val) || p.rho.val != 0) {
+    return use_exp ? transmatexp(p, m) : transmat(p, m);
+  } else {
+    return transvac(p, m);
   }
 }
 
-function oscillate(p, m) {
+function transvac(p, m) {
   var nu = [0, 0, 0];
   nu[p.nu.val] = 1;
-  // const conv = 2.534; // Conversion factor from natural to useful units.
   const conv = 1e-6 / ((2 * 1.0545718e-34 * 299792458) / 1.602e-19); // Conversion factor from natural to useful units.
   var Hexp = math.multiply(
     math.complex(0, 1),
@@ -170,145 +181,42 @@ function oscillate(p, m) {
   return math.re(math.dotMultiply(UHUdnu, math.conj(UHUdnu))).valueOf();
 }
 
-// class Oscillator {
-//   private:
-// 	// Neutrino oscillation parameter struct.
-//   OscPars op;
+function transmat(p, m) {
+  var nu = [0, 0, 0];
+  nu[p.nu.val] = 1;
 
-//   // Oscillation matrix.
-//   Eigen::Matrix3cd U;
-//   Eigen::Matrix3cd Ud;
-//   // Hamiltonian and matter potential.
-//   Eigen::Matrix3cd H;
-//   Eigen::Matrix3cd V;
-//   Eigen::Matrix3cd Hexp;
-//   Eigen::Matrix3cd Vexp;
-//   // Mass difference matrix.
-//   Eigen::Matrix3d Dmsq;
+  const conv = 1e-6 / ((2 * 1.0545718e-34 * 299792458) / 1.602e-19); // Conversion factor from natural to useful units.
+  const N = 128; // Large enough N for Lie product formula.
+  // Temporary Hamiltonian to component-wise exponentiate.
+  var Hexp = math.multiply(
+    math.complex(0, 1),
+    (-1 * conv * p.L.val) / p.E.val / N,
+    m.H
+  );
+  for (var j = 0; j < 3; ++j) Hexp._data[j][j] = math.exp(Hexp.get([j, j]));
+  var Vexp = math.multiply(math.complex(0, 1), m.V, (-1 * p.L.val) / N);
+  Vexp._data[0][0] = math.exp(Vexp.get([0, 0]));
+  Vexp._data[1][1] = 1;
+  Vexp._data[2][2] = 1;
+  // Slow matrix power. Better than exponential...
+  const HUdVUpow = math.pow(math.multiply(Hexp, m.Ud, Vexp, m.U), N);
+  // console.log(HUdVUpow);
+  const UHUdnu = math.multiply(m.U, HUdVUpow, m.Ud, nu);
+  return math.re(math.dotMultiply(UHUdnu, math.conj(UHUdnu))).valueOf();
+}
 
-//   public:
-//   Oscillator() {
-//     update();
-//   } // Oscillator::Oscillator
+function transmatexp(p, m) {
+  var nu = [0, 0, 0];
+  nu[p.nu.val] = 1;
 
-//   void update() {
-//     const double s12 = sin(op.th12);
-//     const double s23 = sin(op.th23);
-//     const double s13 = sin(op.th13);
-//     const double c12 = cos(op.th12);
-//     const double c23 = cos(op.th23);
-//     const double c13 = cos(op.th13);
-
-//     // Chirality
-//     const double ch = (int)op.anti * -2 + 1; // (-)1 if (anti)neutrino
-
-//     // Construct mixing matrix.
-//     Eigen::Matrix3cd U1;
-//     U1 << 1, 0, 0,
-//           0, c23, s23,
-//           0, -s23, c23;
-//     Eigen::Matrix3cd U2;
-//     U2 << c13, 0, s13*exp(-If*ch*op.dCP),
-//           0, 1, 0,
-//           -s13*exp(If*ch*op.dCP), 0, c13;
-//     Eigen::Matrix3cd U3;
-//     U3 << c12, s12, 0,
-//           -s12, c12, 0,
-//           0, 0, 1;
-//     U = U1*U2*U3;
-//     Ud = U.adjoint();
-
-//     // Hamiltonian and matter potential.
-//     H << 0, 0, 0,
-//          0, op.Dm21sq, 0,
-//          0, 0, op.Dm31sq;
-
-//     const double Gf = 4.54164e-37; // Reduced Fermi constant * (c*hbar)^2 in m^2.
-//     const double Ne = op.rho/(1.672e-27)/2; // Electron number density in m^-3.
-//     V(0,0) = ch*sqrt(2)*Gf*Ne * 1e3; // Multiply and convert to km^-1.
-
-//   } // Oscillator::update()
-
-//   // Expose the neutrino oscillation parameter set to mess with it.
-//   OscPars& pars() { return op; }
-
-//   // General transformation function that decides between vacuum and matter oscillation.
-//   Eigen::Vector3d trans(bool using_exp = false) const {
-//     if(op.rho==0) {
-//       return transvac();
-//     }
-//     return using_exp? transmatexp(): transmat();
-//   } // Oscillator::trans()
-
-//   // Analytical determination of neutrino oscillation using Hamiltonian.
-//   Eigen::Vector3d transvac() const {
-//     Eigen::Vector3cd nu(0,0,0);
-//     nu(op.nu) = 1;
-//     const double conv = 2.534; // Conversion factor from natural to useful units.
-//     Eigen::Matrix3cd Hexp = -If*H/op.E*conv*op.L; // Temporary Hamiltonian to component-wise exponentiate.
-//     for(int j=0; j<3; ++j) Hexp(j,j) = exp(Hexp(j,j));
-//     return (U*Hexp*Ud*nu).cwiseAbs2();
-//   } // Oscillator::transvac()
-
-//   // Analytical neutrino oscillation in matter using Hamiltonian.
-//   Eigen::Vector3d transmat() const {
-//     Eigen::Vector3cd nu(0,0,0);
-//     nu(op.nu) = 1;
-//     // Propagate.
-//     const double conv = 2.534; // Conversion factor from natural to useful units.
-//     const int N = 128; // Large enough N for Lie product formula.
-//     Eigen::Matrix3cd Hexp = -If*H/op.E*conv*op.L/N; // Temporary Hamiltonian to component-wise exponentiate.
-//     for(int j=0; j<3; ++j) Hexp(j,j) = exp(Hexp(j,j));
-//     Eigen::Matrix3cd Vexp = -If*V*op.L/N; // Temporary matter potential to component-wise exponentiate.
-//     for(int j=0; j<3; ++j) Vexp(j,j) = exp(Vexp(j,j));
-//     // Slow matrix power. Better than exponential...
-//     Eigen::MatrixPower<Eigen::Matrix3cd> Apow(Hexp*Ud*Vexp*U);
-//     return (U*Apow(N)*Ud*nu).cwiseAbs2();
-//   } // Oscillator::transmat()
-
-//   // Analytical neutrino oscillation in matter using Hamiltonian, using matrix exponential.
-//   Eigen::Vector3d transmatexp() const {
-//     Eigen::Vector3cd nu(0,0,0);
-//     nu(op.nu) = 1;
-//     // Propagate.
-//     const double conv = 2.534; // Conversion factor from natural to useful units.
-//     const Eigen::Matrix3cd Htmp = -If*H/op.E*conv*op.L; // Temporary Hamiltonian.
-//     const Eigen::Matrix3cd Vtmp = -If*V*op.L; // Temporary matter potential.
-//     return (U*(Htmp+Ud*Vtmp*U).exp()*Ud*nu).cwiseAbs2();
-//   } // Oscillator::transmat()
-// }; // class Oscillator
-
-// // Function to export neutrino oscillation data to csv.
-// void exportData(const std::vector<Eigen::Vector3d>& probs, const double final) {
-//   const std::string filename = "nu.csv";
-//   std::ofstream ofile(filename);
-//   if(!ofile.is_open()) {
-//     std::cout << "Couldn't create file " << filename << ".\n";
-//     return;
-//   }
-
-//   // Header.
-//   ofile << "x,nue,numu,nutau\n";
-//   // Record probabilities as function of variable x, either E or L.
-//   for(double i = 0; i < probs.size(); ++i) {
-//     ofile << i*(final/probs.size()) << ',' << probs[i](0) << ',' << probs[i](1) << ',' << probs[i](2) << '\n';
-//   }
-//   std::cout << "Saving to " << filename << ".\n";
-//   ofile.close();
-// }
-
-// // Function to obtain a range of neutrino oscillation probabilities vs a parameter.
-// std::vector<Eigen::Vector3d> oscillate(neutosc::Oscillator& osc, double& par, int numsteps = 1000, bool using_exp = false) {
-//   const double initial = par;
-//   const double step = initial/numsteps;
-//   std::vector<Eigen::Vector3d> result(numsteps+1);
-//   // Propagate.
-//   for(int i=0; i<result.size(); ++i) {
-//     par = i*step;
-//     osc.update();
-//     result[i] = osc.trans(using_exp);
-//   }
-//   // Reset to original parameter value to avoid rounding errors.
-//   par = initial;
-//   return result;
-// } // std::vector<Eigen::Vector3d> oscillate()
+  const conv = 1e-6 / ((2 * 1.0545718e-34 * 299792458) / 1.602e-19); // Conversion factor from natural to useful units.
+  const Htmp = math.multiply(
+    math.complex(0, -1),
+    m.H,
+    (conv * p.L.val) / p.E.val
+  );
+  const Vtmp = math.multiply(math.complex(0, -1), m.V, p.L.val);
+  const Hmat = math.add(Htmp, math.multiply(m.Ud, Vtmp, m.U));
+  const UHUdnu = math.multiply(m.U, math.expm(Hmat), m.Ud, nu);
+  return math.re(math.dotMultiply(UHUdnu, math.conj(UHUdnu))).valueOf();
+}
